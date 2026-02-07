fcf8f2ec937526a2ec0c28df5c56d003b24f322f
elisheva413 <eli7p654321@gmail.com>
2026-02-07 22:07:40 +0200

    Start Project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
-
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
+        providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
